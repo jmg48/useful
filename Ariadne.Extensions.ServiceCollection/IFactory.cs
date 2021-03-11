41ebb3fc8a47ba63a7a9e6e24e98446766854820
Jon Glass <jon.glass@esosolutions.com>
2021-03-11 08:40:56 +0000

    Add XmlDoc comments

--- Ariadne.Extensions.ServiceCollection/IFactory.cs
+++ Ariadne.Extensions.ServiceCollection/IFactory.cs
@@ -1,22 +1,44 @@
 ﻿namespace Ariadne.Extensions.ServiceCollection
 {
-    public interface IFactory<out TResult>
+    /// <summary>
+    /// Specifies the contract for an abstract factory, allowing instances of <see cref="TService"/> to be created on demand. 
+    /// </summary>
+    /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+    public interface IFactory<out TService>
     {
-        TResult New();
+        TService New();
     }
 
-    public interface IFactory<in T, out TResult>
+    /// <summary>
+    /// Specifies the contract for an abstract factory, allowing instances of <see cref="TService"/> to be created on demand. 
+    /// </summary>
+    /// <typeparam name="T">The type of the parameter used to create service instances.</typeparam>
+    /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+    public interface IFactory<in T, out TService>
     {
-        TResult New(T arg);
+        TService New(T arg);
     }
 
-    public interface IFactory<in T1, in T2, out TResult>
+    /// <summary>
+    /// Specifies the contract for an abstract factory, allowing instances of <see cref="TService"/> to be created on demand. 
+    /// </summary>
+    /// <typeparam name="T1">The type of the first parameter used to create service instances.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter used to create service instances.</typeparam>
+    /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+    public interface IFactory<in T1, in T2, out TService>
     {
-        TResult New(T1 arg1, T2 arg2);
+        TService New(T1 arg1, T2 arg2);
     }
 
-    public interface IFactory<in T1, in T2, in T3, out TResult>
+    /// <summary>
+    /// Specifies the contract for an abstract factory, allowing instances of <see cref="TService"/> to be created on demand. 
+    /// </summary>
+    /// <typeparam name="T1">The type of the first parameter used to create service instances.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter used to create service instances.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter used to create service instances.</typeparam>
+    /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+    public interface IFactory<in T1, in T2, in T3, out TService>
     {
-        TResult New(T1 arg1, T2 arg2, T3 arg3);
+        TService New(T1 arg1, T2 arg2, T3 arg3);
     }
 }
--- Ariadne.Extensions.ServiceCollection/ServiceCollectionExtensions.cs
+++ Ariadne.Extensions.ServiceCollection/ServiceCollectionExtensions.cs
@@ -6,22 +6,49 @@
 
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Add a factory service for <see cref="IFactory{TService}"/>, allowing other services to resolve instances of <see cref="TService"/> on demand.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
         public static void AddFactory<TService>(this IServiceCollection services)
-            where TService : class
-        {
+            where TService : class =>
             services.AddSingleton<IFactory<TService>>(x => new Factory<TService>(x.GetService<TService>));
-        }
 
+        /// <summary>
+        /// Add a factory service for <see cref="IFactory{T, TService}"/>, allowing other services to create instances of <see cref="TService"/> on demand.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter used to create service instances.</typeparam>
+        /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
         public static void AddFactory<T, TService, TImplementation>(this IServiceCollection services)
             where TService : class
             where TImplementation : class, TService =>
             services.AddSingleton(Factory<T, TService, TImplementation>);
 
+        /// <summary>
+        /// Add a factory service for <see cref="IFactory{T1, T2, TService}"/>, allowing other services to create instances of <see cref="TService"/> on demand.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter used to create service instances.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter used to create service instances.</typeparam>
+        /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
         public static void AddFactory<T1, T2, TService, TImplementation>(this IServiceCollection services)
             where TService : class
             where TImplementation : class, TService =>
             services.AddSingleton(Factory<T1, T2, TService, TImplementation>);
 
+        /// <summary>
+        /// Add a factory service for <see cref="IFactory{T1, T2, T3, TService}"/>, allowing other services to create instances of <see cref="TService"/> on demand.
+        /// </summary>
+        /// <typeparam name="T1">The type of the first parameter used to create service instances.</typeparam>
+        /// <typeparam name="T2">The type of the second parameter used to create service instances.</typeparam>
+        /// <typeparam name="T3">The type of the third parameter used to create service instances.</typeparam>
+        /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
         public static void AddFactory<T1, T2, T3, TService, TImplementation>(this IServiceCollection services)
             where TService : class
             where TImplementation : class, TService =>
@@ -32,7 +59,8 @@
             // n.b. Resolve everything inside the factory delegate, in order to respect lifestyles of resolved services
             var constructor = typeof(TImplementation).GetConstructors().Single();
             var parameters = constructor.GetParameters();
-            return new Factory<T, TService>(t => (TService)constructor.Invoke(parameters.Take(parameters.Length - 1).Select(parameter => serviceProvider.GetRequiredService(parameter.ParameterType)).Concat(new object[] { t }).ToArray()));
+            return new Factory<T, TService>(t => (TService) constructor.Invoke(parameters.Take(parameters.Length - 1)
+                .Select(parameter => serviceProvider.GetRequiredService(parameter.ParameterType)).Concat(new object[] {t}).ToArray()));
         }
 
         private static IFactory<T1, T2, TService> Factory<T1, T2, TService, TImplementation>(IServiceProvider serviceProvider)
@@ -40,7 +68,8 @@
             // n.b. Resolve everything inside the factory delegate, in order to respect lifestyles of resolved services
             var constructor = typeof(TImplementation).GetConstructors().Single();
             var parameters = constructor.GetParameters();
-            return new Factory<T1, T2, TService>((t1, t2) => (TService)constructor.Invoke(parameters.Take(parameters.Length - 2).Select(parameter => serviceProvider.GetRequiredService(parameter.ParameterType)).Concat(new object[] { t1, t2 }).ToArray()));
+            return new Factory<T1, T2, TService>((t1, t2) => (TService) constructor.Invoke(parameters.Take(parameters.Length - 2)
+                .Select(parameter => serviceProvider.GetRequiredService(parameter.ParameterType)).Concat(new object[] {t1, t2}).ToArray()));
         }
 
         private static IFactory<T1, T2, T3, TService> Factory<T1, T2, T3, TService, TImplementation>(IServiceProvider serviceProvider)
@@ -48,7 +77,8 @@
             // n.b. Resolve everything inside the factory delegate, in order to respect lifestyles of resolved services
             var constructor = typeof(TImplementation).GetConstructors().Single();
             var parameters = constructor.GetParameters();
-            return new Factory<T1, T2, T3, TService>((t1, t2, t3) => (TService)constructor.Invoke(parameters.Take(parameters.Length - 3).Select(parameter => serviceProvider.GetRequiredService(parameter.ParameterType)).Concat(new object[] { t1, t2, t3 }).ToArray()));
+            return new Factory<T1, T2, T3, TService>((t1, t2, t3) => (TService) constructor.Invoke(parameters.Take(parameters.Length - 3)
+                .Select(parameter => serviceProvider.GetRequiredService(parameter.ParameterType)).Concat(new object[] {t1, t2, t3}).ToArray()));
         }
     }
 }
