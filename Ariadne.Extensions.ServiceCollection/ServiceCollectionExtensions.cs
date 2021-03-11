38e484c5035f0d6e3190f25569de6fef37b5105f
Jon Glass <jon.glass@esosolutions.com>
2021-03-11 13:31:29 +0000

    Return service collection to allow fluent usage

--- Ariadne.Extensions.ServiceCollection/ServiceCollectionExtensions.cs
+++ Ariadne.Extensions.ServiceCollection/ServiceCollectionExtensions.cs
@@ -7,41 +7,44 @@
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// Add a factory service for <see cref="IFactory{TService}"/>, allowing other services to resolve instances of <see cref="TService"/> on demand.
+        /// Add a factory service for <see cref="IFactory{TService}"/>, allowing other services to resolve instances of TService on demand.
         /// </summary>
         /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
         /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
-        public static void AddFactory<TService>(this IServiceCollection services)
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddFactory<TService>(this IServiceCollection services)
             where TService : class =>
             services.AddSingleton<IFactory<TService>>(x => new Factory<TService>(x.GetService<TService>));
 
         /// <summary>
-        /// Add a factory service for <see cref="IFactory{T, TService}"/>, allowing other services to create instances of <see cref="TService"/> on demand.
+        /// Add a factory service for <see cref="IFactory{T, TService}"/>, allowing other services to create instances of TService on demand.
         /// </summary>
         /// <typeparam name="T">The type of the parameter used to create service instances.</typeparam>
         /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
         /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
         /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
-        public static void AddFactory<T, TService, TImplementation>(this IServiceCollection services)
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddFactory<T, TService, TImplementation>(this IServiceCollection services)
             where TService : class
             where TImplementation : class, TService =>
             services.AddSingleton(Factory<T, TService, TImplementation>);
 
         /// <summary>
-        /// Add a factory service for <see cref="IFactory{T1, T2, TService}"/>, allowing other services to create instances of <see cref="TService"/> on demand.
+        /// Add a factory service for <see cref="IFactory{T1, T2, TService}"/>, allowing other services to create instances of TService on demand.
         /// </summary>
         /// <typeparam name="T1">The type of the first parameter used to create service instances.</typeparam>
         /// <typeparam name="T2">The type of the second parameter used to create service instances.</typeparam>
         /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
         /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
         /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
-        public static void AddFactory<T1, T2, TService, TImplementation>(this IServiceCollection services)
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddFactory<T1, T2, TService, TImplementation>(this IServiceCollection services)
             where TService : class
             where TImplementation : class, TService =>
             services.AddSingleton(Factory<T1, T2, TService, TImplementation>);
 
         /// <summary>
-        /// Add a factory service for <see cref="IFactory{T1, T2, T3, TService}"/>, allowing other services to create instances of <see cref="TService"/> on demand.
+        /// Add a factory service for <see cref="IFactory{T1, T2, T3, TService}"/>, allowing other services to create instances of TService on demand.
         /// </summary>
         /// <typeparam name="T1">The type of the first parameter used to create service instances.</typeparam>
         /// <typeparam name="T2">The type of the second parameter used to create service instances.</typeparam>
@@ -49,7 +52,8 @@
         /// <typeparam name="TService">The type of the service to be created by the factory.</typeparam>
         /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
         /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the factory service to.</param>
-        public static void AddFactory<T1, T2, T3, TService, TImplementation>(this IServiceCollection services)
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddFactory<T1, T2, T3, TService, TImplementation>(this IServiceCollection services)
             where TService : class
             where TImplementation : class, TService =>
             services.AddSingleton(Factory<T1, T2, T3, TService, TImplementation>);
